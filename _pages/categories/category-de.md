48b5a6be2203d3b982808dcffc73ca84da2d7401
Chanwoo Park <chanwoopark@Chanwoos-MacBook-Pro.local>
2022-02-08 02:28:53 +0900

    1차 포스트

---
+++ _pages/categories/category-de.md
@@ -0,0 +1,14 @@
+---
+title: "Data Engineering"
+layout: archive
+permalink: categories/de
+author_profile: true
+sidebar_main: true
+---
+
+<!-- 공백이 포함되어 있는 카테고리 이름의 경우 site.categories['a b c'] 이런식으로! -->
+
+***
+
+{% assign posts = site.categories.de %}
+{% for post in posts %} {% include archive-single2.html type=page.entries_layout %} {% endfor %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,99 +5,21 @@ var store = [{
         "url": "http://localhost:4000/example_sub/test/",
         "teaser": null
       },{
-        "title": "C++ Chapter 20.1 : Visual Studio에서 프로파일링 하기",
-        "excerpt":"인프런에 있는 홍정모 교수님의 홍정모의 따라 하며 배우는 C++ 강의를 듣고 정리한 필기입니다. 😀 🌜 [홍정모의 따라 하며 배우는 C++]강의 들으러 가기! chapter 20. 중급 프로그래머들의 상식 Visual Studio에서 프로파일링 하기 프로 파일링 👉 CPU 사용량을 알아보는 일 디버그 모드로 바꾼다. 🔴🔴 break point 를 찍어 디버깅 할 범위를 설정...","categories": ["Cpp"],
-        "tags": ["Programming","Cpp"],
-        "url": "http://localhost:4000/cpp/chapter20-1/",
+        "title": "파이썬, 자료구조",
+        "excerpt":"서론 옛날에 RTOS관련 대학원에 진학해보려 했다가 받은 질문중에 하나가 스택과 큐의 차이점과 예시를 설명하라는 내용이었다. 스택은 쌓아놓은 책을 위에서 부터 다시꺼낸다면 큐는 만화책 반납통같이 위에서 넣으면 밑에서 꺼내는 개념이라고 말씀드렸다. 물론 틀린건 아니지만 지금 생각해보면 그냥 얘는 개념만 알고 실사용은 해보지 않은 친구라고 생각하셨을 것 같다. 오늘 각각을 구현해보면서 이해해보는...","categories": ["Something_else"],
+        "tags": ["algorithm","data_structure"],
+        "url": "http://localhost:4000/something_else/4th/",
         "teaser": null
       },{
-        "title": "C++ Chapter 20.2 : vcpkg 설치 방법",
-        "excerpt":"인프런에 있는 홍정모 교수님의 홍정모의 따라 하며 배우는 C++ 강의를 듣고 정리한 필기입니다. 😀 🌜 [홍정모의 따라 하며 배우는 C++]강의 들으러 가기! chapter 20. 중급 프로그래머들의 상식 vepkg 외부의 여러 라이브러리 설치를 도와준다 vcpkg 설치 방법 vcpkg Github 저장소로 가서 quick start를 보고 따라서 설치하면 된다. vcpkg를 사용하여 외부 라이브러리...","categories": ["Cpp"],
-        "tags": ["Programming","Cpp"],
-        "url": "http://localhost:4000/cpp/chapter20-2/",
+        "title": "진짜 시작 그리고 m1맥북 세팅",
+        "excerpt":"반성 지난 6월 코드스테이츠를 시작하면서 블로그를 개설했는데 꾸준히 쓰겠다고 했는데, 꾸준히는 개뿔 이게 테스트용 첫번째 글 이후 이게 5개월만에 두번째 글을 올리게 되었다. 왜 필요성을 느꼈는가 사실 대학교를 다닐때도 기록보단 필기를 하였고, 시험공부를 위한 필기였기에 그 필요성을 느끼지 못하다가 이번에 section4 딥러닝 파트를 마무리하면서 그 필요성을 느끼게 되었다. 그 전환점으로...","categories": ["Something_else"],
+        "tags": ["setting","M1"],
+        "url": "http://localhost:4000/something_else/setting/",
         "teaser": null
       },{
-        "title": "C++ Chapter 20.3 : 외부 라이브러리 설치, 프로젝트 템플릿",
-        "excerpt":"인프런에 있는 홍정모 교수님의 홍정모의 따라 하며 배우는 C++ 강의를 듣고 정리한 필기입니다. 😀 🌜 [홍정모의 따라 하며 배우는 C++]강의 들으러 가기! chapter 20. 중급 프로그래머들의 상식 외부 라이브러리 설치해보기 연습 삼아 NanoGUI 라는 라이브러리를 설치한다고 가정해보자. 이 라이브러리는 라이브러리들을 많이 사용하고 있어서 설치하기 가장 복잡한 사례다. 첫 번째 방법...","categories": ["Cpp"],
-        "tags": ["Programming","Cpp"],
-        "url": "http://localhost:4000/cpp/chapter20-3/",
-        "teaser": null
-      },{
-        "title": "UE4 Chapter 10-3. HUD : 보스 HP Bar",
-        "excerpt":"인프런에 있는 황대희님의 언리얼 엔진4 3D 횡스크롤 게임 만들기 강의를 듣고 정리한 필기입니다. 😀 언리얼 엔진 공식 문서 https://docs.unrealengine.com/ko/index.html 🚖 보스의 HP Bar UI 만들기 보스에게 가까이 가면 플레이어처럼 우측 상단에 보스의 HP Bar 를 띄우도록 한다. Canvas Panel을 하나 더 추가해서 Canvas Panel 하나만 보이게 하면 보스의 UI 들이...","categories": ["UE4 Lesson 1"],
-        "tags": ["Game Engine","UE4"],
-        "url": "http://localhost:4000/ue4%20lesson%201/ch10-3/",
-        "teaser": null
-      },{
-        "title": "UE4 Chapter 10-4. HUD : 몬스터 데미지 Text + TimeLine",
-        "excerpt":"인프런에 있는 황대희님의 언리얼 엔진4 3D 횡스크롤 게임 만들기 강의를 듣고 정리한 필기입니다. 😀 언리얼 엔진 공식 문서 https://docs.unrealengine.com/ko/index.html 🚖 몬스터 데미지 Text 액터 만들고 몬스터 위에 띄우기 Text 액터 몬스터를 때릴 때마다 들어간 데미지가 숫자 Text 로 뜨게 하자. 메이플처럼! 🚩 “CharDamageTextBP” 액터 블루프린트 뷰포트 몬스터 Hp Bar 와...","categories": ["UE4 Lesson 1"],
-        "tags": ["Game Engine","UE4"],
-        "url": "http://localhost:4000/ue4%20lesson%201/ch10-4/",
-        "teaser": null
-      },{
-        "title": "UE4 Chapter 11-1. 시네마틱 : 시네마틱 영상 제작",
-        "excerpt":"인프런에 있는 황대희님의 언리얼 엔진4 3D 횡스크롤 게임 만들기 강의를 듣고 정리한 필기입니다. 😀 언리얼 엔진 공식 문서 https://docs.unrealengine.com/ko/index.html 🚖 시네마틱 영상 제작 시퀀서 시네마틱 영상은 장면(Shot)들로 구성이 되어 있다. 장면을 나누는 기준은 촬영하는 카메라의 위치와 각도가 변하는 것을 기준으로 한다. 하나의 장면은 트랙(Track)으로 구성이 되어 있다. 시퀀스 종류 레벨...","categories": ["UE4 Lesson 1"],
-        "tags": ["Game Engine","UE4"],
-        "url": "http://localhost:4000/ue4%20lesson%201/ch11-1/",
-        "teaser": null
-      },{
-        "title": "UE4 Chapter 11-2. 시네마틱 : 게임에서 영상 플레이하기",
-        "excerpt":"인프런에 있는 황대희님의 언리얼 엔진4 3D 횡스크롤 게임 만들기 강의를 듣고 정리한 필기입니다. 😀 언리얼 엔진 공식 문서 https://docs.unrealengine.com/ko/index.html 🚖 게임에서 영상 플레이하기 플레이어가 보스 근처에 가면 자동으로 이전에 만든 시네마틱 영상이 플레이 되게끔 하기. 👉 보스가 있는 지점에 충돌 체크를 해야 함. 충돌이 일어나면 영상 플레이. 🚩 “BossCinematicTrigger” 액터...","categories": ["UE4 Lesson 1"],
-        "tags": ["Game Engine","UE4"],
-        "url": "http://localhost:4000/ue4%20lesson%201/ch11-2/",
-        "teaser": null
-      },{
-        "title": "UE4 Chapter 12. 구르기",
-        "excerpt":"인프런에 있는 황대희님의 언리얼 엔진4 3D 횡스크롤 게임 만들기 강의를 듣고 정리한 필기입니다. 😀 언리얼 엔진 공식 문서 https://docs.unrealengine.com/ko/index.html 🚖 구르기 애니메이션 구르기 애니메이션 Root Motion 을 활성화 한다. 구르는 동작이 플레이어의 위치 변화에 영향을 줄 수 있도록. 구르기 애님 몽타주 위 애니메이션으로 애님 몽타주 생성 구르기 모션이 시작되는 재생...","categories": ["UE4 Lesson 1"],
-        "tags": ["Game Engine","UE4"],
-        "url": "http://localhost:4000/ue4%20lesson%201/ch12/",
-        "teaser": null
-      },{
-        "title": "UE4 Chapter 13. 버그 수정 : 공격시엔 180도로 회전시키기",
-        "excerpt":"인프런에 있는 황대희님의 언리얼 엔진4 3D 횡스크롤 게임 만들기 강의를 듣고 정리한 필기입니다. 😀 언리얼 엔진 공식 문서 https://docs.unrealengine.com/ko/index.html 🚖 정확히 180도 회전하게 만들기 플레이어 캐릭터가 A, D 키에 의하여 방향을 바꾸는데 횡스크롤 게임이므로 180도로 확 돌려야 한다. 근데 A, D 키를 조금만 누르면 90도쯤 밖에 회전이 안되서 앞 뒤...","categories": ["UE4 Lesson 1"],
-        "tags": ["Game Engine","UE4"],
-        "url": "http://localhost:4000/ue4%20lesson%201/ch13/",
-        "teaser": null
-      },{
-        "title": "UE4 Chapter 14. 애니메이션 재생시 파티클 이펙트 효과",
-        "excerpt":"인프런에 있는 황대희님의 언리얼 엔진4 3D 횡스크롤 게임 만들기 강의를 듣고 정리한 필기입니다. 😀 언리얼 엔진 공식 문서 https://docs.unrealengine.com/ko/index.html 🚖 검기 이펙트 검을 휘두를 때마다 파티클 효과 재생시킬 것. 다운 받은 검기 이펙트 언리얼 프로젝트를 열어 사용할 폴더를 우클 - 이주(Migration) 하여 내 프로젝트로 복사. 🚩 플레이어 공격 애니메이션 애님...","categories": ["UE4 Lesson 1"],
-        "tags": ["Game Engine","UE4"],
-        "url": "http://localhost:4000/ue4%20lesson%201/ch14/",
-        "teaser": null
-      },{
-        "title": "UE4 Chapter 15. 장애물 만들기",
-        "excerpt":"인프런에 있는 황대희님의 언리얼 엔진4 3D 횡스크롤 게임 만들기 강의를 듣고 정리한 필기입니다. 😀 언리얼 엔진 공식 문서 https://docs.unrealengine.com/ko/index.html 🚖 장애물 만들기 장애물 액터를 여러개 복사하여 배치한 후 이를 모두 선택하여 우클 - 액터-&gt;스태틱 메시로 변환 해주면 이 여러개의 액터가 하나의 스태틱 메시가 된다. 하나의 장애물 에셋이 됨. 🚖 장애물에...","categories": ["UE4 Lesson 1"],
-        "tags": ["Game Engine","UE4"],
-        "url": "http://localhost:4000/ue4%20lesson%201/ch15/",
-        "teaser": null
-      },{
-        "title": "C++ 포인터와 다차원 배열 (int[col] 배열을 가리키는 포인터)",
-        "excerpt":"         내가 쓴 답변인데 정리할 겸..        🌜 개인 공부 기록용 블로그입니다. 오류나 틀린 부분이 있을 경우  언제든지 댓글 혹은 메일로 지적해주시면 감사하겠습니다! 😄   맨 위로 이동하기  ","categories": ["Cpp"],
-        "tags": ["Programming","Cpp"],
-        "url": "http://localhost:4000/cpp/2array/",
-        "teaser": null
-      },{
-        "title": "C++ const (컴파일 상수/런타임 상수) 그리고 constexpr",
-        "excerpt":"🚀 C에서의 const const int size = 2; int arr[size]; // ❌ C에선 안된다. C 언어에서 const는 런타임 상수이다. 런타임 상수라는 의미는 const인 size 공간 안에 2 가 할당 되는 것은 컴파일까지 다 마친 후 프로그램이 실행 될 때 이루어진다는 것이다. C언어에서는 const가 오로지 런타임 상수이기 때문에 const로 배열의 크기를...","categories": ["Cpp"],
-        "tags": ["Programming","Cpp"],
-        "url": "http://localhost:4000/cpp/const/",
-        "teaser": null
-      },{
-        "title": "C++ scanf 와 cin 의 입출력 속도 비교, 입출력 속도 높이기",
-        "excerpt":"🚀 C의 입출력 함수(scanf, printf)와 C++의 입출력 함수(cin, count) cin, cout 은 scanf, printf 보다 2 배 이상 느리다. 따라서 입출력을 해야하는 코딩테스트 문제를 풀이할 때 이점을 염두해두고 풀이해야 한다. 그냥 scanf, printf 사용하기 cin, cout 을 사용할 것이라면 C와 C++ 사이의 버퍼 동기화를 끊는다. 백준 문제들은 프로그래머스와 다르게 직접...","categories": ["Cpp"],
-        "tags": ["Programming","Cpp"],
-        "url": "http://localhost:4000/cpp/iospeed/",
-        "teaser": null
-      },{
-        "title": "C++ 코딩테스트 꿀팁! 테스트 케이스 한 번에 입력하기 (백준 저지에 유용)",
-        "excerpt":"🚀 일일이 콘솔창에 입력하는 것은 너무 귀찮다 프로그래머스와 달리 백준 저지는 직접 입출력 함수를 사용하여 입출력을 받아야했기 때문에 IDE 에서 테스트 실행시에도, 디버깅시에도 문제에서 주어진 예제 테케를 일일이 콘솔창에 입력을 해주어야해서 너무 불편하다고 느꼈다. 일일이 타이핑하여 콘솔 창에 입력하지 않고 텍스트 파일에 테스트 케이스 전체를 복사해두고 이로부터 파일 입력을 받도록...","categories": ["Cpp"],
-        "tags": ["Programming","Cpp"],
-        "url": "http://localhost:4000/cpp/freopen/",
-        "teaser": null
-      },{
-        "title": "C++ 코딩테스트 꿀팁! 헤더 인클루딩 타이핑이 귀찮을 때 : #include <bits/stdc++.h>",
-        "excerpt":"🚀 #include &lt;bits/stdc++.h&gt; 🔥 장점 헤더 인클루딩 일일이 쓰기가 귀찮을 때! 나는 코딩테스트 답안 코드를 Visual Studio 에서 작성하고 있는데, 코드를 새로 작성할 때마다 #include 헤더를 인클루딩 하기가 귀찮으니 저렇게 내가 자주 사용하는 헤더들을 무더기로 미리 써놓고 사용하고 있었다. 그러던 와중에 다른 분들의 코테 답안에서 bits/stdc++.h 이 헤더를 인클루딩 한게...","categories": ["Cpp"],
-        "tags": ["Programming","Cpp"],
-        "url": "http://localhost:4000/cpp/stdc/",
+        "title": "openCV 이미지 읽기",
+        "excerpt":"OpenCV vs pillow(PIL) vs scikit-image https://github.com/ethereon/lycon 위 링크에서는 세 라이브러리의 속도비교를 해 놓았는데, 속도측이나 기능측이나 빠르 openCV를 사용한 이미지 읽기를 정리해보려한다. 만약 다른 라이블리르 이용할 일이 생길때마다 업데이트를 해야겠다. openCV 설치 conda install -c anaconda opencv local 경로로 이미지열기 import cv2 cv2.imread('파일경로', flags) # ndarray형식으로 리턴. flags 는 컬러 (3차원)가...","categories": ["ds"],
+        "tags": ["Image","OpenCV"],
+        "url": "http://localhost:4000/ds/3rd/",
         "teaser": null
       }]
